5c61f81439ec33dfda722cf96a3f79571ffa8b83
Suji Kim <rlatnwl9595@naver.com>
2018-11-24 20:49:24 +0900

    initial commit

---
+++ Bluemoon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bluemoon
+//
+//  Created by Suji Kim on 24/11/2018.
+//  Copyright © 2018 mashup6th. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
