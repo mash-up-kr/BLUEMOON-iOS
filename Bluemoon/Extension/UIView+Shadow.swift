f0405ae7a0580954f1d7da60bc248893c6379ad4
Gaon Kim <gaon.kim.dev@gmail.com>
2018-11-25 00:06:39 +0900

    UIView + Shadow

---
+++ Bluemoon/Extension/UIView+Shadow.swift
@@ -0,0 +1,52 @@
+//
+//  UIView+Shadow.swift
+//  Bluemoon
+//
+//  Created by Gaon Kim on 24/11/2018.
+//  Copyright © 2018 mashup6th. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    /// SwifterSwift: Shadow color of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowColor: UIColor? {
+        get {
+            guard let color = layer.shadowColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+
+    /// SwifterSwift: Shadow offset of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+
+    /// SwifterSwift: Shadow opacity of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    /// SwifterSwift: Shadow radius of view; also inspectable from Storyboard.
+    @IBInspectable public var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+}
